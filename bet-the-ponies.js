066f138685c2cfda80435de95732c9ac0e0fe8b5
BranceA <brancealexander@gmail.com>
2019-09-02 13:00:34 -0500

    horse objects made and named

--- bet-the-ponies.js
+++ bet-the-ponies.js
@@ -1,21 +1,177 @@
 var startButton = document.getElementById('start');
 var testHorse = document.getElementById('test-horse');
 
-// $("#start").click(function () {
-//     $("#test-horse").animate({left: "200px"}, 1000);
-// });
-
 $("button").click(function(){
     console.log("this works");
-    var firstInterval = Math.floor(Math.random() * (2000 - 100 +1));
-    var secondInterval = Math.floor(Math.random() * (2000 - 100 +1));
-    var thirdInterval = Math.floor(Math.random() * (2000 - 100 +1));
-    var fourthInterval = Math.floor(Math.random() * (2000 - 100 +1));
-    var fifthInterval = Math.floor(Math.random() * (2000 - 100 +1));
-    $("#test-horse").animate({left: "+=20%"}, firstInterval);
-    $("#test-horse").animate({left: "+=20%"}, secondInterval);
-    $("#test-horse").animate({left: "+=20%"}, thirdInterval);
-    $("#test-horse").animate({left: "+=20%"}, fourthInterval);
-    $("#test-horse").animate({left: "+=20%"}, fifthInterval);
+    var firstInterval = Math.floor(Math.random() * (2000 - 800) + 800);
+    var secondInterval = Math.floor(Math.random() * (2000 - 800) + 800);
+    var thirdInterval = Math.floor(Math.random() * (2000 - 800) + 800);
+    var fourthInterval = Math.floor(Math.random() * (2000 - 800) + 800);
+    var fifthInterval = Math.floor(Math.random() * (2000 - 800) + 800);
+    var intervalTotal = firstInterval + secondInterval + thirdInterval + fourthInterval + fifthInterval;
+    $("#test-horse").animate({left: "+=19vw"}, firstInterval);
+    $("#test-horse").animate({left: "+=19vw"}, secondInterval);
+    $("#test-horse").animate({left: "+=19vw"}, thirdInterval);
+    $("#test-horse").animate({left: "+=19vw"}, fourthInterval);
+    $("#test-horse").animate({left: "+=19vw"}, fifthInterval);
+    var timeoutId = setTimeout(function () {
+        $("footer").append("<p>WE DID IT!!!</p><p>" + intervalTotal + "</p>");
+    }, intervalTotal)
+
+    console.log("this works");
+    var firstIntervalTwo = Math.floor(Math.random() * (2200 - 1000 ) + 1000);
+    var secondIntervalTwo = Math.floor(Math.random() * (2200 - 1000 ) + 1000);
+    var thirdIntervalTwo = Math.floor(Math.random() * (2200 - 1000 ) + 1000);
+    var fourthIntervalTwo = Math.floor(Math.random() * (2200 - 1000 ) + 1000);
+    var fifthIntervalTwo = Math.floor(Math.random() * (2200 - 1000 ) + 1000);
+    var intervalTotalTwo = firstIntervalTwo + secondIntervalTwo + thirdIntervalTwo + fourthIntervalTwo + fifthIntervalTwo;
+    $("#test-horse-two").animate({left: "+=19vw"}, firstIntervalTwo);
+    $("#test-horse-two").animate({left: "+=19vw"}, secondIntervalTwo);
+    $("#test-horse-two").animate({left: "+=19vw"}, thirdIntervalTwo);
+    $("#test-horse-two").animate({left: "+=19vw"}, fourthIntervalTwo);
+    $("#test-horse-two").animate({left: "+=19vw"}, fifthIntervalTwo);
+    var timeoutId = setTimeout(function () {
+        $("footer").append("<p>WE DID IT TWO!!!</p><p>" + intervalTotalTwo + "</p>");
+    }, intervalTotalTwo)
 });
+
+var horseNumberOne = {
+    name: "David Bowie",
+    minInterval: 1000,
+    maxInterval: 2000,
+    odds: 1
+};
+
+var horseNumberTwo = {
+    name: "Warren Zevon",
+    minInterval: 800,
+    maxInterval: 2000,
+    odds: 2
+};
+
+var horseNumberThree = {
+    name: "Pete Townshend",
+    minInterval: 1000,
+    maxInterval: 2300,
+    odds: 2
+};
+
+var horseNumberFour = {
+    name: "Keith Richards",
+    minInterval: 1000,
+    maxInterval: 2000,
+    odds: 2
+};
+
+var horseNumberFive = {
+    name: "Billy Joel",
+    minInterval: 1000,
+    maxInterval: 2000,
+    odds: 2
+};
+
+var horseNumberSix = {
+    name: "Neil Peart",
+    minInterval: 1000,
+    maxInterval: 2500,
+    odds: 2
+};
+
+var horseNumberSeven = {
+    name: "Freddie Mercury",
+    minInterval: 1000,
+    maxInterval: 2000,
+    odds: 2
+};
+
+var horseNumberEight = {
+    name: "Jimi Hendrix",
+    minInterval: 1000,
+    maxInterval: 2000,
+    odds: 2
+};
+
+var horseNumberNine = {
+    name: "Gene Simmons",
+    minInterval: 1000,
+    maxInterval: 2000,
+    odds: 2
+};
+
+var horseNumberTen = {
+    name: "Ted Nugent",
+    minInterval: 1000,
+    maxInterval: 2000,
+    odds: 2
+};
+
+var horseNumberEleven = {
+    name: "Daryl Hall",
+    minInterval: 1000,
+    maxInterval: 2000,
+    odds: 2
+};
+
+var horseNumberTwelve = {
+    name: "John Oates",
+    minInterval: 1000,
+    maxInterval: 2000,
+    odds: 2
+};
+
+var horseNumberThirteen = {
+    name: "Meatloaf",
+    minInterval: 1000,
+    maxInterval: 2000,
+    odds: 2
+};
+
+var horseNumberFourteen = {
+    name: "Stevie Nicks",
+    minInterval: 1000,
+    maxInterval: 2000,
+    odds: 2
+};
+
+var horseNumberFifteen = {
+    name: "Eddie Van Halen",
+    minInterval: 1000,
+    maxInterval: 2000,
+    odds: 2
+};
+
+var horseNumberSixteen = {
+    name: "Tom Petty",
+    minInterval: 1000,
+    maxInterval: 2000,
+    odds: 2
+};
+
+var horseNumberSeventeen = {
+    name: "Jim Morrison",
+    minInterval: 1000,
+    maxInterval: 2000,
+    odds: 2
+};
+
+var horseNumberEighteen = {
+    name: "Ringo Starr",
+    minInterval: 1000,
+    maxInterval: 2000,
+    odds: 2
+};
+
+var horseNumberNineteen = {
+    name: "Lars Ulrich",
+    minInterval: 1000,
+    maxInterval: 2000,
+    odds: 2
+};
+
+var horseNumberTwenty = {
+    name: "Sammy Hagar",
+    minInterval: 1000,
+    maxInterval: 2000,
+    odds: 2
+};
 
