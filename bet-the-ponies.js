89ea0eef4fc4f3968064a98aca24fd3bdfebbd6b
BranceA <brancealexander@gmail.com>
2019-09-02 13:30:11 -0500

    can successfully add random horse name to ul

--- bet-the-ponies.js
+++ bet-the-ponies.js
@@ -1,40 +1,6 @@
 var startButton = document.getElementById('start');
 var testHorse = document.getElementById('test-horse');
 
-$("button").click(function(){
-    console.log("this works");
-    var firstInterval = Math.floor(Math.random() * (2000 - 800) + 800);
-    var secondInterval = Math.floor(Math.random() * (2000 - 800) + 800);
-    var thirdInterval = Math.floor(Math.random() * (2000 - 800) + 800);
-    var fourthInterval = Math.floor(Math.random() * (2000 - 800) + 800);
-    var fifthInterval = Math.floor(Math.random() * (2000 - 800) + 800);
-    var intervalTotal = firstInterval + secondInterval + thirdInterval + fourthInterval + fifthInterval;
-    $("#test-horse").animate({left: "+=19vw"}, firstInterval);
-    $("#test-horse").animate({left: "+=19vw"}, secondInterval);
-    $("#test-horse").animate({left: "+=19vw"}, thirdInterval);
-    $("#test-horse").animate({left: "+=19vw"}, fourthInterval);
-    $("#test-horse").animate({left: "+=19vw"}, fifthInterval);
-    var timeoutId = setTimeout(function () {
-        $("footer").append("<p>WE DID IT!!!</p><p>" + intervalTotal + "</p>");
-    }, intervalTotal)
-
-    console.log("this works");
-    var firstIntervalTwo = Math.floor(Math.random() * (2200 - 1000 ) + 1000);
-    var secondIntervalTwo = Math.floor(Math.random() * (2200 - 1000 ) + 1000);
-    var thirdIntervalTwo = Math.floor(Math.random() * (2200 - 1000 ) + 1000);
-    var fourthIntervalTwo = Math.floor(Math.random() * (2200 - 1000 ) + 1000);
-    var fifthIntervalTwo = Math.floor(Math.random() * (2200 - 1000 ) + 1000);
-    var intervalTotalTwo = firstIntervalTwo + secondIntervalTwo + thirdIntervalTwo + fourthIntervalTwo + fifthIntervalTwo;
-    $("#test-horse-two").animate({left: "+=19vw"}, firstIntervalTwo);
-    $("#test-horse-two").animate({left: "+=19vw"}, secondIntervalTwo);
-    $("#test-horse-two").animate({left: "+=19vw"}, thirdIntervalTwo);
-    $("#test-horse-two").animate({left: "+=19vw"}, fourthIntervalTwo);
-    $("#test-horse-two").animate({left: "+=19vw"}, fifthIntervalTwo);
-    var timeoutId = setTimeout(function () {
-        $("footer").append("<p>WE DID IT TWO!!!</p><p>" + intervalTotalTwo + "</p>");
-    }, intervalTotalTwo)
-});
-
 var horseNumberOne = {
     name: "David Bowie",
     minInterval: 1000,
@@ -176,3 +142,46 @@ var horseNumberTwenty = {
 };
 
 var horseArray = [horseNumberOne, horseNumberTwo, horseNumberThree, horseNumberFour, horseNumberFive, horseNumberSix, horseNumberSeven, horseNumberEight, horseNumberNine, horseNumberTen, horseNumberEleven, horseNumberTwelve, horseNumberThirteen, horseNumberFourteen, horseNumberFifteen, horseNumberSixteen, horseNumberSeventeen, horseNumberEighteen, horseNumberNineteen, horseNumberTwenty];
+
+function pickRandomHorses () {
+    var laneOneHorse = horseArray[Math.floor(Math.random() * 20)];
+    $("#horse-1-li").text(laneOneHorse.name);
+}
+
+pickRandomHorses();
+
+$("button").click(function(){
+    console.log("this works");
+    var firstInterval = Math.floor(Math.random() * (2000 - 800) + 800);
+    var secondInterval = Math.floor(Math.random() * (2000 - 800) + 800);
+    var thirdInterval = Math.floor(Math.random() * (2000 - 800) + 800);
+    var fourthInterval = Math.floor(Math.random() * (2000 - 800) + 800);
+    var fifthInterval = Math.floor(Math.random() * (2000 - 800) + 800);
+    var intervalTotal = firstInterval + secondInterval + thirdInterval + fourthInterval + fifthInterval;
+    $("#test-horse").animate({left: "+=19vw"}, firstInterval);
+    $("#test-horse").animate({left: "+=19vw"}, secondInterval);
+    $("#test-horse").animate({left: "+=19vw"}, thirdInterval);
+    $("#test-horse").animate({left: "+=19vw"}, fourthInterval);
+    $("#test-horse").animate({left: "+=19vw"}, fifthInterval);
+    var timeoutId = setTimeout(function () {
+        $("footer").append("<p>WE DID IT!!!</p><p>" + intervalTotal + "</p>");
+    }, intervalTotal);
+
+    console.log("this works");
+    var firstIntervalTwo = Math.floor(Math.random() * (2200 - 1000 ) + 1000);
+    var secondIntervalTwo = Math.floor(Math.random() * (2200 - 1000 ) + 1000);
+    var thirdIntervalTwo = Math.floor(Math.random() * (2200 - 1000 ) + 1000);
+    var fourthIntervalTwo = Math.floor(Math.random() * (2200 - 1000 ) + 1000);
+    var fifthIntervalTwo = Math.floor(Math.random() * (2200 - 1000 ) + 1000);
+    var intervalTotalTwo = firstIntervalTwo + secondIntervalTwo + thirdIntervalTwo + fourthIntervalTwo + fifthIntervalTwo;
+    $("#test-horse-two").animate({left: "+=19vw"}, firstIntervalTwo);
+    $("#test-horse-two").animate({left: "+=19vw"}, secondIntervalTwo);
+    $("#test-horse-two").animate({left: "+=19vw"}, thirdIntervalTwo);
+    $("#test-horse-two").animate({left: "+=19vw"}, fourthIntervalTwo);
+    $("#test-horse-two").animate({left: "+=19vw"}, fifthIntervalTwo);
+    var timeoutId = setTimeout(function () {
+        $("footer").append("<p>WE DID IT TWO!!!</p><p>" + intervalTotalTwo + "</p>");
+    }, intervalTotalTwo)
+});
+
+
